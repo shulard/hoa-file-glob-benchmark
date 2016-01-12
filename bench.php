c7550a7f64f7d8d28b80082943f72f30724a17e8
Stéphane HULARD <s.hulard@chstudio.fr>
2016-01-12 18:46:03 +0000

    Add bench script

---
+++ bench.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__.'/FinderGlob.php';
+require_once __DIR__.'/FinderGlobIterator.php';
+
+use Hoa\File\Finder;
+
+$toBench = function(Finder $finder) use ($argv) {
+    $i = 0;
+    $finder->in($argv[1])->maxDepth(5);
+    $it = $finder->getIterator();
+    foreach( $it as $path ) {
+        $i++;
+    }
+    var_dump($i);
+};
+$bench = new Hoa\Bench;
+
+$bench->glob->start();
+call_user_func($toBench, new FinderGlob);
+$bench->glob->stop();
+usleep(1000);
+$bench->iterator->start();
+call_user_func($toBench, new FinderGlobIterator);
+$bench->iterator->stop();
+
+// Print statistics.
+echo $bench;
